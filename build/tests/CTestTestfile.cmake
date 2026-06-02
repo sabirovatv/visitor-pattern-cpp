14307aa707f4a753443047fe58c92aec34944d21
sabirovatv <sabirovatamara77@gmail.com>
2026-06-02 09:28:40 +0300

    Initial commit: Visitor pattern with CMake, test, and CI

--- build/tests/CTestTestfile.cmake
+++ build/tests/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(VisitorTest "/home/toma/visitor-pattern-project/build/tests/test_visitor")
-set_tests_properties(VisitorTest PROPERTIES  _BACKTRACE_TRIPLES "/home/toma/visitor-pattern-project/tests/CMakeLists.txt;9;add_test;/home/toma/visitor-pattern-project/tests/CMakeLists.txt;0;")
+set_tests_properties(VisitorTest PROPERTIES  _BACKTRACE_TRIPLES "/home/toma/visitor-pattern-project/tests/CMakeLists.txt;7;add_test;/home/toma/visitor-pattern-project/tests/CMakeLists.txt;0;")
